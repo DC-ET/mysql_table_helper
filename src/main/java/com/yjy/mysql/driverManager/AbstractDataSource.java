681f0072531d1945b31b8e55c681f907239b2f1d
yjy <1335823608@qq.com>
2017-09-22 11:20:27 +0800

    1.优化代码, 使用 slf4j

--- src/main/java/com/yjy/mysql/driverManager/AbstractDataSource.java
+++ src/main/java/com/yjy/mysql/driverManager/AbstractDataSource.java
@@ -1,7 +1,8 @@
 package com.yjy.mysql.driverManager;
 
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
 import java.io.PrintWriter;
@@ -13,7 +14,7 @@ import java.sql.SQLException;
  */
 public abstract class AbstractDataSource implements DataSource {
 
-    protected final Logger logger = Logger.getLogger(AbstractDataSource.class);
+    protected final Logger logger = LoggerFactory.getLogger(AbstractDataSource.class);
 
     public AbstractDataSource() {
     }
